81d0177473fa70eed8e835928c9e08b795b260a0
AlexAlmeidaLeonardo <aal2005@gmail.com>
2024-11-10 02:26:40 -0300

    Ajustes no app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,11 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    success_message = request.args.get('success')
+    return render_template('index.html', success=success_message)
 
-@app.route('/formulario')
+@app.route('/formulario_antigo')
 def formulario():
     return render_template('formulario.html')
+
+@app.route('/formulario')
+def novo_formulario():
+    return render_template('novo_formulario.html')
 
 @app.route('/submit_course_registration', methods=['POST'])
 def submit_course_registration():
@@ -24,28 +29,26 @@ def submit_course_registration():
     # ---------------- que serão utilizados em produção! ----------------
     # -------------------------------------------------------------------
     db = mysql.connector.connect(
-         host="AlexAlmeidaLeonardo.mysql.pythonanywhere-services.com",
-         user="AlexAlmeidaLeona",
-         password="vo_ambrosina_2024",  # Se a senha estiver em branco, deixe assim
-         database="AlexAlmeidaLeona$vo_ambrosina",
+         host="dados_secretos_dados_secretos_dados_secretos_dados_secretos",
+         user="dados_secretos_dados_secretos_dados_secretos_dados_secretos",
+         password="dados_secretos_dados_secretos_dados_secretos_dados_secretos",
+         database="dados_secretos_dados_secretos_dados_secretos_dados_secretos",
          port=3306  # Use a porta 3306, que é a padrão do MySQL no XAMPP
      )
 
-    
     try:
-        # Recebendo os dados do formulário
         data = request.form
-        nome = data.get('name')
+        nome = data.get('nome')
         email = data.get('email')
-        telefone = data.get('phone')
-        data_nascimento = data.get('dob')
+        telefone = data.get('telefone')
+        data_nascimento = data.get('data_nascimento')
         cep = data.get('cep')
         rua = data.get('rua')
         numero = data.get('numero')
         cidade = data.get('cidade')
         estado = data.get('estado')
         oficina = data.get('course')
-        mensagem = data.get('message')
+        mensagem = data.get('observacoes')
 
         # Conectando ao banco de dados
         cursor = db.cursor()
@@ -60,11 +63,12 @@ def submit_course_registration():
         cursor.execute(query, values)
         db.commit()
 
-        return jsonify({"message": "Inscrição realizada com sucesso!"}), 200
+        return redirect(url_for('home', success="Inscrição realizada com sucesso!"))
 
     except mysql.connector.Error as err:
         print(f"Erro: {err}")
-        return jsonify({"message": "Erro ao realizar inscrição."}), 500
+        mensagem = jsonify({"message": "Erro ao realizar inscrição."}), 500
+        return '', 204
 
     finally:
         if cursor:
